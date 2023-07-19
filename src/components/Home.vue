400f8ab67464733be967105db7adec8e6459848f
李帅 <lishuai1116@digiwin.com>
2023-07-19 18:06:19 +0800

    🎉 init

---
+++ src/components/Home.vue
@@ -0,0 +1,128 @@
+<script setup>
+import {ref} from "vue";
+
+const policeFilingHref = ref('http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011502011885')
+const policeFilingText = ref(`苏公网安备 32011502011885号`)
+const domainFilingHref = ref('https://beian.miit.gov.cn/')
+const domainFilingText = ref(`苏ICP备2023021194号-1`)
+const types = ref([
+  {type: 'Node', url:'https://registry.npmmirror.com/binary.html?path=node/'},
+  {type: 'Git', url:'https://registry.npmmirror.com/binary.html?path=git-for-windows/'},
+  {type: 'Jetbrain', url:'https://jetbra.in/s'},
+  {type: 'windows', url:'https://github.com/TGSAN/CMWTAT_Digital_Edition'}
+])
+const access = (item) => {
+  window.open(item.url)
+}
+</script>
+<template>
+  <div class="home">
+    <div class="title"></div>
+    <el-scrollbar style="height:75vh; min-height: 225px;">
+      <div class="entrance_box">
+        <div v-for="(item, index) of types" :key="item.type" class='entrance_item'>
+          <div class="item_title">{{ item.type }}</div>
+          <div :class="['item_icon', `item_icon_${index+1}`]"></div>
+          <el-button @click="access(item)" class="access-btn" size="large" color="#2D71FB" plain>立即访问</el-button>
+        </div>
+      </div>
+    </el-scrollbar>
+    <el-affix position="bottom" :offset="1">
+      <div class="footer nine-color">
+      <span>关于超越 |
+        <el-link :href="policeFilingHref" target='_blank' class="margin-12-right" type="info">
+          <img class="gonganbeian" src="../assets/img/gonganbeian.png"  alt=""/>{{ policeFilingText }}
+        </el-link>
+        <el-link :href="domainFilingHref" target='_blank' type="info">{{ domainFilingText }}</el-link>
+        2023 个人版权所有
+      </span>
+      </div>
+    </el-affix>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  overflow: hidden;
+}
+.title {
+  top: 0;
+  width: 29%;
+  height: 17.5%;
+  background: url("../assets/img/title.jpg") no-repeat;
+  background-size: 100% 100%;
+  position: relative;
+  z-index: 1;
+}
+
+
+.entrance_box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  column-gap: 12px;
+  row-gap: 18px;
+  width: 100vw;
+  margin-top: 50px;
+  padding-left: 5vw;
+  .entrance_item {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    padding: 14px;
+    height: 100%;
+    width: 20%;
+    min-width: 220px;
+    background: #FFFFFF;
+    box-shadow: -9px 1px 24px 0 #9f9f9f1a;
+    border-radius: 8px;
+    &:hover {
+      background: url("../assets/img/hover_bg.png") no-repeat;
+      background-size: 100% 100%;
+    }
+    .item_title {
+      color: #000000;
+      font-weight: 600;
+      font-size: 20px;
+    }
+    .access-btn {
+      margin-top: 12px;
+    }
+  }
+}
+
+.item_icon {
+  width: 120px;
+  height: 120px;
+}
+.item_icon_1 {
+  background: url("../assets/img/item_icon_1.png") no-repeat;
+  background-size: 100% 100%;
+}
+.item_icon_2 {
+  background: url("../assets/img/item_icon_2.png") no-repeat;
+  background-size: 100% 100%;
+}
+.item_icon_3 {
+  background: url("../assets/img/item_icon_3.png") no-repeat;
+  background-size: 100% 100%;
+}
+.item_icon_4 {
+  background: url("../assets/img/item_icon_4.png") no-repeat;
+  background-size: 100% 100%;
+}
+.footer {
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  span {
+    padding-top: 6px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+//引入element-plus
+import ElementPlus from 'element-plus'
+//引入element-plus相关样式
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
